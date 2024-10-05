9ca31f4b06de2f98ce9d0571f6aec0c73e45ef19
GitHubNexon <134722556+GitHubNexon@users.noreply.github.com>
2024-10-05 09:29:49 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://test-client-livid.vercel.app/",
+    origin: "https://test-client-livid.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
